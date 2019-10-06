beeeeb9955591c1931d5c592cac3a7957f70c24e
Andres G. Aragoneses <knocte@gmail.com>
2019-10-06 16:29:18 +0800

    with VS but only uwp

--- configure.bat
+++ configure.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Workload.NetCrossPlat --passive --norestart
+REM "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Workload.NetCrossPlat --passive --norestart
 "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Workload.Universal --passive --norestart

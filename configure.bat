2c20865e372cfc47e95e90a0803d10fc00e640c4
Andres G. Aragoneses <knocte@gmail.com>
2019-10-06 22:10:03 +0800

    remove unneeded configure.bat

--- configure.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Workload.NetCrossPlat --passive --norestart
-"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" --add Microsoft.VisualStudio.Workload.Universal --passive --norestart
